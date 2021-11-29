e64caecc1fbf6c3407a26bd8b14479cecb8643c8
Benjamin Aziz Khalil Ajayi <kabbaben@hotmail.co.uk>
2021-11-29 21:05:27 +0000

    Implemented the basis of block functionality

---
+++ vidibolt/src/core/block.cpp
@@ -0,0 +1,105 @@
+#include <core/block.h>
+#include <crypto/sha256.h>
+
+namespace Volt
+{
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	class Block::Implementation
+	{
+	public:
+		uint32_t index;
+		uint64_t timestamp, nonce;
+		std::string previousHash, hash;
+		std::vector<Transaction> txs;
+	public:
+		Implementation() :
+			index(0), timestamp(0), nonce(0)
+		{}
+
+		Implementation(uint32_t index, uint64_t timestamp, const std::string& prevHash, const std::vector<Transaction>& txs,
+			const std::string& blockHash) :
+			index(index), timestamp(timestamp), previousHash(prevHash), txs(txs), hash(blockHash), nonce(0)
+		{}
+
+		~Implementation() = default;
+	};
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+	Block::Block() :
+		impl(std::make_unique<Implementation>())
+	{}
+
+	Block::Block(const Block& block) :
+		impl(std::make_unique<Implementation>(block.GetIndex(), block.GetTimestamp(), block.GetPreviousBlockHash(),
+			block.GetTransactions(), block.GetBlockHash()))
+	{}
+
+	Block::Block(uint32_t index, uint64_t timestamp, const std::string& prevHash, const std::vector<Transaction>& txs,
+		const std::string& blockHash) :
+		impl(std::make_unique<Implementation>(index, timestamp, prevHash, txs, blockHash))
+	{}
+
+	Block::~Block() = default;
+
+	void Block::operator=(const Block& block)
+	{
+		this->impl = std::make_unique<Implementation>(block.GetIndex(), block.GetTimestamp(), block.GetPreviousBlockHash(),
+			block.GetTransactions(), block.GetBlockHash());
+	}
+
+	ErrorID Block::GenerateBlockHash()
+	{
+		// Push the data from all transactions in the block into the string
+		std::string combinedTxsData;
+		for (const auto& tx : this->impl->txs)
+			combinedTxsData += Volt::SerializeTransaction(tx);
+
+		// Combine all block data into one string, then convert string data into byte vector array
+		const std::string blockData = std::to_string(this->impl->index) + std::to_string(this->impl->timestamp) +
+			std::to_string(this->impl->nonce) + this->impl->previousHash + combinedTxsData;
+
+		const std::vector<uint8_t> rawBlockData = Volt::GetRawString(blockData);
+
+		// Generate the hash
+		std::vector<uint8_t> rawHashDigest;
+		ErrorID error = Volt::GetSHA256Digest(rawBlockData, rawHashDigest);
+		if (error == ErrorID::NONE)
+			this->impl->hash = Volt::ConvertByteToHexData(rawHashDigest);
+
+		return error;
+	}
+
+	const uint32_t& Block::GetIndex() const
+	{
+		return this->impl->index;
+	}
+
+	const uint64_t& Block::GetTimestamp() const
+	{
+		return this->impl->timestamp;
+	}
+
+	const uint64_t& Block::GetNonce() const
+	{
+		return this->impl->nonce;
+	}
+
+	const std::string& Block::GetPreviousBlockHash() const
+	{
+		return this->impl->previousHash;
+	}
+
+	const std::string& Block::GetBlockHash() const
+	{
+		return this->impl->hash;
+	}
+
+	const std::vector<Transaction>& Block::GetTransactions() const
+	{
+		return this->impl->txs;
+	}
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+}
---
+++ vidibolt/src/core/block.h
@@ -0,0 +1,69 @@
+#ifndef VIDIBOLT_CORE_BLOCK_H
+#define VIDIBOLT_CORE_BLOCK_H
+
+#include <util/volt_api.h>
+#include <core/transaction.h>
+
+#include <string>
+#include <vector>
+#include <memory>
+
+namespace Volt
+{
+	class Block
+	{
+	private:
+		class Implementation;
+		std::unique_ptr<Implementation> impl;
+	public:
+		VOLT_API Block();
+		VOLT_API Block(const Block& block);
+		VOLT_API Block(uint32_t index, uint64_t timestamp, const std::string& prevHash, const std::vector<Transaction>& txs,
+			const std::string& blockHash = "");
+
+		VOLT_API ~Block();
+		
+		/*
+			Operator overload for assignment operations.
+		*/
+		VOLT_API void operator=(const Block& block);
+
+		/*
+			Generates the hash of the block based on its contents. 
+			Returns error ID if an error occurs.
+		*/
+		VOLT_API ErrorID GenerateBlockHash();
+
+		/*
+			Returns the index of the block.
+		*/
+		VOLT_API const uint32_t& GetIndex() const;
+
+		/*
+			Returns the timestamp of the block.
+		*/
+		VOLT_API const uint64_t& GetTimestamp() const;
+
+		/*
+			Returns the nonce value of the block.
+		*/
+		VOLT_API const uint64_t& GetNonce() const;
+
+		/*
+			Returns the block's previous hash.
+		*/
+		VOLT_API const std::string& GetPreviousBlockHash() const;
+
+		/*
+			Returns the hash of the block.
+		*/
+		VOLT_API const std::string& GetBlockHash() const;
+
+		/*
+			Returns the transactions contained in the block.
+		*/
+		VOLT_API const std::vector<Transaction>& GetTransactions() const;
+	};
+}
+
+#endif
